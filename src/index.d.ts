d83192fde177277296c9233090d631d99108626a
cAttte <26514199+cAttte@users.noreply.github.com>
2020-12-01 13:23:35 -0300

    feat: create declaration file

---
+++ src/index.d.ts
@@ -0,0 +1,15 @@
+import winston from "winston"
+
+type LogLevel = "info" | "error" | "warn" | "debug"
+
+type BotLoggerOptions = {
+    logFiles: boolean
+    consoleLevel: LogLevel
+    fileLevel: LogLevel
+    filePath: string
+    fileFrequency: string
+}
+
+export default function createLogger(
+    options: winston.LoggerOptions & BotLoggerOptions
+): winston.Logger
